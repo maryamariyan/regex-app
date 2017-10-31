c7a326743b284dba63d4129849351ed14452c357
Maryam Ariyan <maryam.ariyan@microsoft.com>
2017-10-31 12:01:03 -0400

    adding RemoveHeaderComments, adding sln and formatting code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
- using System.IO;
- using System.Text.RegularExpressions;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace regexapp
 {
@@ -8,57 +8,72 @@ namespace regexapp
     {
         static void Main(string[] args)
         {
-		RemoveMultipleBlankLines();
+            //RemoveMultipleBlankLines();
 
-		RemoveAnnotationLine();
-		RemoveMultipleBlankLinesToNoBlank();
+            //RemoveAnnotationLine();
+            //RemoveMultipleBlankLinesToNoBlank();
 
-		Console.WriteLine("Hello World!");
+            RemoveHeaderComments();
+            RemoveMultipleBlankLinesToNoBlank();
+            Console.WriteLine("Hello World!");
         }
 
-	public static void RemoveMultipleBlankLines()
-	{
-		string[] files = Directory.GetFiles(@"/Users/mariyan/CodeHub/corefx/src/System.ComponentModel.Composition/", "*.cs", SearchOption.AllDirectories);
-		foreach(var item in files)
-		{
-			var text = File.ReadAllText(item);
-			var rgex = new Regex(@"(\r?\n\s*){3,}");
-			var rgex2 = new Regex(@"(\r?\n\s*){3,}");			
-			var res = rgex.Replace(text, Environment.NewLine + Environment.NewLine);
-			if (rgex2.IsMatch(text)) File.WriteAllText(item, res);
-		}
-	}
+        public static void RemoveMultipleBlankLines()
+        {
+            string[] files = Directory.GetFiles(@"/Users/mariyan/CodeHub/corefx/src/System.ComponentModel.Composition/", "*.cs", SearchOption.AllDirectories);
+            foreach (var item in files)
+            {
+                var text = File.ReadAllText(item);
+                var rgex = new Regex(@"(\r?\n\s*){3,}");
+                var rgex2 = new Regex(@"(\r?\n\s*){3,}");
+                var res = rgex.Replace(text, Environment.NewLine + Environment.NewLine);
+                if (rgex2.IsMatch(text)) File.WriteAllText(item, res);
+            }
+        }
 
-	public static void RemoveAnnotationLine()
-	{
-                string[] files = Directory.GetFiles(@"/Users/mariyan/CodeHub/corefx/src/System.ComponentModel.Composition/", "*.cs", SearchOption.AllDirectories);
-                foreach(var item in files)
-                {
-                        var text = File.ReadAllText(item);
-                        var rgex = new Regex(@"[ ]*\[ContractArgumentValidator\]");
-                        var rgex2 = new Regex(@"[ ]*\[ContractArgumentValidator\]");
-                        var res = rgex.Replace(text, Environment.NewLine + Environment.NewLine + Environment.NewLine);
-                        if (rgex2.IsMatch(text)) File.WriteAllText(item, res);
-                }
-	}
+        public static void RemoveAnnotationLine()
+        {
+            string[] files = Directory.GetFiles(@"/Users/mariyan/CodeHub/corefx/src/System.ComponentModel.Composition/", "*.cs", SearchOption.AllDirectories);
+            foreach (var item in files)
+            {
+                var text = File.ReadAllText(item);
+                var rgex = new Regex(@"[ ]*\[ContractArgumentValidator\]");
+                var rgex2 = new Regex(@"[ ]*\[ContractArgumentValidator\]");
+                var res = rgex.Replace(text, Environment.NewLine + Environment.NewLine + Environment.NewLine);
+                if (rgex2.IsMatch(text)) File.WriteAllText(item, res);
+            }
+        }
 
         public static void RemoveMultipleBlankLinesToNoBlank()
         {
-                string[] files = Directory.GetFiles(@"/Users/mariyan/CodeHub/corefx/src/System.ComponentModel.Composition/", "*.cs", SearchOption.AllDirectories);
-                foreach(var item in files)
-                {
-                        var text = File.ReadAllText(item);
-                        var rgex = new Regex(@"(\r?\n\s*){3,}");
-                        var rgex2 = new Regex(@"(\r?\n\s*){3,}");
-                        var res = rgex.Replace(text, Environment.NewLine);
-                        if (rgex2.IsMatch(text)) File.WriteAllText(item, res);
-                }
+            string[] files = Directory.GetFiles(@"C:\CodeHub\corefx\src\System.ComponentModel.Composition\", "*.cs", SearchOption.AllDirectories);
+            foreach (var item in files)
+            {
+                var text = File.ReadAllText(item);
+                var rgex = new Regex(@"(\r?\n\s*){3,}");
+                var rgex2 = new Regex(@"(\r?\n\s*){3,}");
+                var res = rgex.Replace(text, Environment.NewLine);
+                if (rgex2.IsMatch(text)) File.WriteAllText(item, res);
+            }
         }
 
-// TODO:
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+        // TODO:
+        // Licensed to the .NET Foundation under one or more agreements.
+        // The .NET Foundation licenses this file to you under the MIT license.
+        // See the LICENSE file in the project root for more information.
+
+        public static void RemoveHeaderComments()
+        {
+            string[] files = Directory.GetFiles(@"C:\CodeHub\corefx\src\System.ComponentModel.Composition\", "*.cs", SearchOption.AllDirectories);
+            foreach (var item in files)
+            {
+                var text = File.ReadAllText(item);
+                var rgex = new Regex(@"[\/]{2,}[ ][-]+(\r?\n\s*)[\/]{2,}[ ](Copyright \(c\) Microsoft Corporation.  All rights reserved).*(\r?\n\s*)[\/]{2,}[ ][-]+");
+                var rgex2 = new Regex(@"[\/]{2,}[ ][-]+(\r?\n\s*)[\/]{2,}[ ](Copyright \(c\) Microsoft Corporation.  All rights reserved).*(\r?\n\s*)[\/]{2,}[ ][-]+");
+                var res = rgex.Replace(text, Environment.NewLine + Environment.NewLine + Environment.NewLine);
+                if (rgex2.IsMatch(text)) File.WriteAllText(item, res);
+            }
+        }
 
     }
 }
